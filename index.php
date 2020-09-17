d0f49f4c76e6d77878c605d403f2fdd9481d6051
Corentin <corentincouteaux@gmail.com>
2020-09-17 16:43:41 +0200

    Freeze

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php include_once "header.inc.php" ?>
+<h1>Wesh la street</h1>
+<?php include_once "footer.inc.php" ?>
---
+++ model/db/db.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once "db.config.php";
+
+$db = new PDO("mysql:host=localhost:3307;dbname=20INF2PJ_TD21G2", $db_user, $db_pass);
