145fdafa20344381cd3a048ea5667a1c2cd083c9
Corentin <corentincouteaux@gmail.com>
2020-09-23 16:05:47 +0200

    Separation

--- model/Post.class.php
+++ model/Post.class.php
@@ -3,5 +3,76 @@
 class Post {
 
     private $title;
+    private $resume;
+    private $content;
+    private $mot_cle; //Tableau de mots clé
+    private $auteur; //ID de l'auteur
+    private $theme;
+
+    function __construct($title, $resume, $content, $auteur, $mot_cle, $theme){
+        //TODO Insertion dans la BDD
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function setTitle($title){
+        $this->title = $title;
+        //TODO Insertion dans la BDD
+    }
+
+    public function getResume(){
+        return $this->resume;
+    }
+
+    public function setResume($resume){
+        $this->resume = $resume;
+        //TODO Insertion dans la BDD
+    }
+
+    public function getContent(){
+        return $this->content;
+    }
+
+    public function setContent($content){
+        $this->content = $content;
+        //TODO Insertion dans la BDD
+    }
+
+    public function getMotCle(){
+        return $this->mot_cle;
+    }
+
+    public function setMotCle($mot_cle){
+        $this->mot_cle = $mot_cle;
+        //TODO Insertion dans la BDD
+    }
+
+    public function getTheme(){
+        return $this->theme;
+    }
+
+    public function setTheme($theme){
+        $this->theme = $theme;
+        //TODO Insertion dans la BDD
+    }
+
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    function getWriteAccessUser(){
+
+    }
+
+    function addWriteAccessUser(){
+
+    }
+
+    function removeWriteAccessUser(){
+
+    }
 
 }
--- model/User.class.php
+++ model/User.class.php
@@ -6,7 +6,28 @@ class User{
     private $name;
     private $posts;
 
+
+    function __construct(){
+
+    }
+
+    function getPosts(){
+
+    }
+
+    function createPost($title, $resume ,$content, $mot_cle, $theme){
+
+    }
+
+    function getWriteAccessPosts(){
+
+    }
+
     static function connect($name, $passwd){
         $sql = "SELECT id FROM CLIENT WHERE Pseudo_Cli='".$name."' AND Mdp_Cli='".$passwd."'";
+    }
+
+    static function register($name, $passwd){
+
     }
 }
